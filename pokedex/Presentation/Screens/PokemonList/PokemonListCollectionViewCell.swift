330513b5d04d219239fb8f983be316e985387c55
Lee Wilson <leew@apadmi.com>
2024-02-04 23:30:02 +0000

    Show images yayy!

--- pokedex/Presentation/Screens/PokemonList/PokemonListCollectionViewCell.swift
+++ pokedex/Presentation/Screens/PokemonList/PokemonListCollectionViewCell.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import Kingfisher
 
 class PokemonListCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     func bind(with pokemon: PokemonDetails) {
-        nameLabel.text = pokemon.name
+        nameLabel.text = pokemon.name.capitalized
+        nameLabel.textAlignment = .center
+        let imgUrl = URL(string: pokemon.sprites.front_default)
+        imageView.kf.setImage(with: imgUrl)
     }
 }
